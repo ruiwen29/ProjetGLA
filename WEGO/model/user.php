4cbb3897e75f938af9884918c5d42c603df9026c
chenwen <chenwen.xiong00@gmail.com>
2019-06-04 10:41:09 +0200

    soutenance

--- WEGO/model/user.php
+++ WEGO/model/user.php
@@ -74,10 +74,12 @@ Class User
 		}
 		else {
 			
-			$row  =  mysqli_fetch_assoc($result);
-			$adr  = $row['Trajet'];
-			echo "<li><a href = '../view/v_trajet.php?adr=$adr' >".$adr ."</a>  <a href = '../control/c_supprimer.php?adr=$adr' > Supprimer</a></li>";
-			//echo "<li>".$adr ."</li>";
+			 while ($row = mysqli_fetch_assoc($result)) {
+				 $adr  = $row['Trajet'];
+				echo "<li><a href = '../view/v_trajet.php?adr=$adr' >".$adr ."</a>  <a href = '../control/c_supprimer.php?adr=$adr' > Supprimer</a></li>";
+				//echo "<li>".$adr ."</li>";
+			 }
+			
 		
 			
 		}
